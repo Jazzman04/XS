9e342e42a2dca6157d065f0da67a7f1ae36f1c3a
Scott Ray <rayscottdaniel@gmail.com>
2026-04-07 00:27:35 -0600

    Created a dataset for swordfighter stats

--- Assets/Scripts/SO_CharacterStats.cs
+++ Assets/Scripts/SO_CharacterStats.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
+[CreateAssetMenu(fileName = "NewCharacterData", menuName = "RPG/Character Data")]
 public class NewScriptableObjectScript : ScriptableObject
 {
+    [Header("Name")]
     public string name; //Character Name
 
+    [Header("Base stats")]
     public int baseStrength, baseConstitution, baseWill, baseWit, baseSpeed, baseSkill, baseLuck; //Base skill levels for character
 
     [Header("Growth Rates")]
+    [Tooltip("Percent chance for a stat to increase. 0.0f = 0%, 1.0f = 100%")]
     // Percent chance for a stat increase 1.0f = 100%, 0.5f = 50% etc.
     public float strengthGrowth;
     public float conGrowth;
